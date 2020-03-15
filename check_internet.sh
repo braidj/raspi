bd2a2c8a9877cf95dc10d87443d61ba0897aa4c6
Jason Braid <braidj@gmail.com>
2020-03-15 17:16:24 +0000

    Forget to stage changes

--- check_internet.sh
+++ check_internet.sh
@@ -1,6 +1,25 @@
-# Author: braidj
-# Date: 20161001
-# Purpose: to check the internet connection
+#------------------------------------------
+# Created: braidj@gmail.com 20200315
+# Purpose: Check the internet connection
+# 2 Optional args: nos_pings, www address
+#------------------------------------------
+
 nos_times="$1"
-sudo ping -c $nos_times www.bbc.co.uk
-return 0
+addr="$2"
+
+default_nos=5
+default_addr="www.bbc.co.uk"
+
+if [ -z "$1"]
+  then
+    nos_times=$default_nos
+fi
+
+if [ -z "$2"]
+  then
+    addr=$default_addr
+fi
+
+echo "Testing with ${nos_times} pings to: $addr"
+sudo ping -c $nos_times $addr
+exit 0
